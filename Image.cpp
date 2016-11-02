b5115176886c0f70d68a12fc8ea21c7163fcb1b2
Schavras <stavros.z@hotmail.com>
2016-11-02 02:15:36 +0200

    Refactored/ resized changed /

--- Image.cpp
+++ Image.cpp
@@ -6,102 +6,138 @@
 #include <algorithm>
 #include <iostream>
 #include "header/Image.h"
+#include "header/Color.h"
 
 using namespace imaging;
 
-Image::Image() {
-    width = 0;
-    height = 0;
-    buffer = nullptr;
-}
-
-Image::Image(unsigned int width, unsigned int height) {
-    Image::width = width;
-    Image::height = height;
-    Image::buffer = new component_t[3 * width * height];
-}
-
-Image::Image(unsigned int width, unsigned int height, const component_t *data_ptr) {
-    Image::width = width;
-    Image::height = height;
-    Image::buffer = new component_t[3 * width * height];
-    setData(data_ptr);
-}
-
-Image::Image(const Image &src) {
-    Image::width = src.width;
-    Image::height = src.height;
-    Image::buffer = new component_t[3 * width * height];
-    setData((const component_t *&) src.buffer);
-}
-
 component_t *Image::getRawDataPtr() {
+    //Returning the pointer
     return buffer;
 }
 
 Color Image::getPixel(unsigned int x, unsigned int y) const {
-    int index = 3 * y + 3 * x * width;
+    /*
+      Since every pixel has 3 values, we need to
+      find the row (3 * y * total width
+      and move 3 * x columns right
+     */
+    int index = 3 * x + 3 * y * getWidth();
 
-    if (index < 0 || index > 3 * width * height) {
+    //checking if index is out of bounds
+    if (index < 0 || index > 3 * getWidth() * getHeight()) {
+        //if out of bounds, return null
         return Color(0, 0, 0);
     }
 
+    //from the starting position, make a new color with the current
+    // and the next 2 values.
     return Color(buffer[index], buffer[index + 1], buffer[index + 2]);
 
 }
 
 void Image::setPixel(unsigned int x, unsigned int y, Color &value) {
 
-    if (x >= height || y >= width) {
+    //Checks if pixel is inside our bounds
+    if (y >= getHeight() || x >= getWidth()) {
         std::cout << "The pixel is outside of the image area" << std::endl;
+        system("PAUSE");
         exit(EXIT_FAILURE);
     }
 
-    int index = 3 * y + 3 * x * width;
+    /*
+     Since every pixel has 3 values, we need to
+     find the row (3 * y * total width
+     and move 3 * x columns right
+    */
+
+    int index = 3 * x + 3 * y * getWidth();
     buffer[index] = value.r;
     buffer[index + 1] = value.g;
     buffer[index + 2] = value.b;
 
 }
-
 void Image::setData(const component_t *&data_ptr) {
 
-    if (!buffer || width == 0 || height == 0) {
-        std::cout << "Width or height are zero values, or buffer is nullptr!" << std::endl;
+    //Checks if image buffer has been allocated
+    //and width and height are not 0
+    if (getRawDataPtr() == nullptr || getHeight() == 0 || getWidth() == 0) {
+        std::cout << "Invalid image" << std::endl;
+        system("PAUSE");
         exit(EXIT_FAILURE);
     }
+
     //Copy Duplicates data from one pointer to another
-    std::copy(data_ptr, data_ptr + 3 * width * height, Image::buffer);
+    //params of copy : (
+    // starting index (data_ptr stores the first memory position),
+    // ending index ( starting position + total number of colors
+    // destination ( the buffer of the image)
+    std::copy(data_ptr, data_ptr + 3 * getWidth() * getHeight(), Image::buffer);
 }
 
 void Image::resize(unsigned int new_width, unsigned int new_height) {
+    //create a new temp buffer with the new dimensions
+    component_t* temp= new  component_t[3 * new_width * new_height];
+    //for the min amount of lines of old and new dimensions
+    for (int i = 0; i < new_height &&  i < getHeight(); i++) {
+        //for the min amount of rows of old and new dimension
+        for (int j = 0; j < new_width && j < getWidth(); j++) {
+            //copy the active data to the temp buffer
+            temp[i*new_width + j] = buffer[i*getWidth() + j];
+        }
+    }
+
+    //delete the old buffer
+    delete[] getRawDataPtr();
+    //allocate new memory with the new dimensions
     Image::width = new_width;
     Image::height = new_height;
     Image::buffer = new component_t[3 * new_width * new_height];
 
-    component_t *newBuffer = new component_t[3 * new_width * new_height];
-
-    for (unsigned int x = 0; x < new_height; x++) {
-        for (unsigned int y = 0; y < new_width; y++) {
-            Color color = getPixel(x, y);
-            int index = 3 * y + 3 * x * new_width;
-            newBuffer[index] = color.r;
-            newBuffer[index + 1] = color.g;
-            newBuffer[index + 2] = color.b;
-        }
-    }
-
-    setData((const component_t *&) newBuffer);
+    //copy the data to the existing bufferr
+    setData((const component_t *&) temp);
 }
 
-Image &operator=(const Image &right) {
-    return *new Image(right);
+//Default constructor.
+Image::Image() {
+    width = 0;
+    height = 0;
+    buffer = nullptr;
 }
 
+//Constructor with width and height specification.
+Image::Image(unsigned int width, unsigned int height) {
+    Image::width = width;
+    Image::height = height;
+    Image::buffer = new component_t[3 * getWidth() * getHeight()];
+}
+
+//Constructor with data initialization.
+Image::Image(unsigned int width, unsigned int height, const component_t *data_ptr) {
+    Image::width = width;
+    Image::height = height;
+    Image::buffer = new component_t[3 * getWidth() * getHeight()];
+    setData(data_ptr);
+}
+
+//Copy constructor.
+Image::Image(const Image &src) {
+    Image::width = src.width;
+    Image::height = src.height;
+    Image::buffer = new component_t[3 * getWidth() * getHeight()];
+    setData((const component_t *&) src.buffer);
+}
+
+//The Image destructor.
 Image::~Image() {
     if (buffer) {
         delete[] buffer;
     }
 }
+
+//Copy assignment operator.
+Image &operator=(const Image &right) {
+    return *new Image(right);
+}
+
 
 
