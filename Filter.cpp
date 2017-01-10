afad8bba9893c4555eecd3c3f780d458f59d3bc6
bdrosatos <esnaueb.app@gmail.com>
2017-01-11 00:04:26 +0200

    initialize new assignment

---
+++ Filter.cpp
@@ -0,0 +1,8 @@
+//
+// Created by bdrosatos on 10/1/2017.
+//
+
+#include "header/Filter.h"
+#include "header/Image.h"
+
+
---
+++ header/Filter.h
@@ -0,0 +1,8 @@
+//
+// Created by bdrosatos on 10/1/2017.
+//
+
+#ifndef PPMPARSER_FILTER_H
+#define PPMPARSER_FILTER_H
+
+#endif //PPMPARSER_FILTER_H
